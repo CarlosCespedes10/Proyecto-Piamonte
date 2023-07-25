56682691abc13cba887d88b080dc89c25fa6d22e
EstebanVera3741 <estebanvera3741@gmail.com>
2023-07-25 10:31:19 -0500

    Piamonte-0000 Crear el formulario de iniciar sesión

--- Piamonte-FrontEnd/src/app/app-routing.module.ts
+++ Piamonte-FrontEnd/src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path:'', redirectTo: '/piamonte-principal-page', pathMatch: 'full'},
   {path:'piamonte-principal-page', loadChildren: () => 
-          import('./module/principal/principal.module').then(m => m.PrincipalModule)}
+          import('./module/principal/principal.module').then(m => m.PrincipalModule)},
+  {path:'piamonte-login-page', loadChildren: () => 
+          import('./module/login/login.module').then(m => m.LoginModule)}
 ];
 
 @NgModule({
